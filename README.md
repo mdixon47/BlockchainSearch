3edbd56edc00bccea136363c40eba2abd51e51a6
Malik Dixon <mdixon47@users.noreply.github.com>
2024-12-28 15:58:41 -0500

    updated files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# blockchainSearch
-Repo for BlockChain Search Engine
+This is a blockchain implementation in Terraform and Python. It is a simple implementation of a blockchain with the following features:   
+
+- Proof of Work
+- Consensus Algorithm
+- Transactions
+- Wallets
+- Mining
+
--- main.tf
+++ main.tf
@@ -16,35 +16,7 @@ terraform {
   }
 }
 
-variable "node_desired_capacity" {
-  description = "Desired number of nodes in the EKS managed node group"
-  type        = number
-  default     = 2
-}
 
-variable "node_max_capacity" {
-  description = "Maximum number of nodes in the EKS managed node group"
-  type        = number
-  default     = 4
-}
-
-variable "node_min_capacity" {
-  description = "Minimum number of nodes in the EKS managed node group"
-  type        = number
-  default     = 1
-}
-
-variable "node_instance_type" {
-  description = "EC2 instance type for the EKS managed node group"
-  type        = string
-  default     = "m5.large"
-}
-
-variable "node_key_name" {
-  description = "Key pair name for the EKS managed node group"
-  type        = string
-  default     = ""
-}
 
 
 provider "aws" {
